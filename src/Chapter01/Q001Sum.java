ac77dba16aa3e142d1a775f77e5e0093a448ffaa
KAispread <pain0121@naver.com>
2022-10-25 17:55:21 +0900

    Initial commit

---
+++ src/Chapter01/Q001Sum.java
@@ -0,0 +1,18 @@
+package Chapter01;
+
+import java.util.Scanner;
+
+public class Q001Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String Num = sc.next();
+        char[] chars = Num.toCharArray();
+
+        int sum = 0;
+        for (char c : chars) {
+            sum += c - '0';
+        }
+        System.out.println(sum);
+    }
+}
