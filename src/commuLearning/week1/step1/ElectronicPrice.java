dc2350684a21df61f0f492a3dd4cd8f1f63f7f9a
KAispread <pain0121@naver.com>
2023-01-15 19:06:15 +0900

    🎲커뮤러닝 1주차: 다시 풀어보기

--- src/commuLearning/week1/step1/ElectronicPrice.java
+++ src/commuLearning/week1/step1/ElectronicPrice.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 * 해결 여부 - X
 * */
 public class ElectronicPrice {
+    public static void main(String[] args) {
+        solution2(new int[][] {{0, 10}, {50, 20}, {100, 30}, {200, 40}}, new int[][] {{3, 50}, {7, 200}, {8, 200}});
+    }
     private int[][] price;
 
     public int solution(int[][] cost, int[][] order) {
@@ -48,6 +51,56 @@ public class ElectronicPrice {
             premonth = month;
         }
         int answer = 123;
+        return answer;
+    }
+
+    public static int solution2(int[][] cost, int[][] order) {
+        int answer = 0;
+
+        int maxMonth = 0;
+        for (int[] o : order) maxMonth = Math.max(maxMonth, o[0]);
+
+        int[] monthlyOrder = new int[maxMonth];
+        int need = 0;
+        int made = 0;
+        for (int[] o : order) {
+            // order 배열이 정렬되어 있지 않음에 주의하세요.
+            monthlyOrder[o[0] - 1] += o[1]; // 월을 index로 사용하기 위해 -1을 해줍니다
+            need += o[1];
+        }
+
+        for (int i = 0; i < cost.length - 1; i++) {
+            if (need == 0 || made >= need) break;
+
+            int price = cost[i][1];
+            int limit = cost[i + 1][0] - cost[i][0];
+            int rest = 0;
+
+            for (int j = 0; j < maxMonth; j++) {
+                if (need == 0 || made >= need) break;
+
+                int making = Math.min(limit, need - made);
+
+                answer += making * price;
+                made += making;
+                need -= monthlyOrder[j];
+
+                if (monthlyOrder[j] == 0) continue; // 납품을 안해도 됩니다.
+
+                int delivery = Math.min(made, monthlyOrder[j]);
+
+                // 납품할때가 되면 만들어놓은 것에서 빼줍니다.
+                made -= delivery;
+                monthlyOrder[j] -= delivery;
+                rest += monthlyOrder[j]; // 납품하고도 남은것은 다음구간에서 만들어야 합니다.
+            }
+
+            need = rest;
+            made = 0;
+        }
+
+        // 나머지 것들은 최종구간의 가격을 적용합니다.
+        answer += need * cost[cost.length - 1][1];
         return answer;
     }
 }
--- src/commuLearning/week1/step4/Question1.java
+++ src/commuLearning/week1/step4/Question1.java
@@ -27,4 +27,22 @@ public class Question1 {
 
         return count;
     }
+
+    static class Solution {
+        public int solution(int[] A, int[] B) {
+            Arrays.sort(A);
+            Arrays.sort(B);
+            int index = B.length - 1;
+
+            int answer = 0;
+            for (int i = A.length - 1; i >= 0; i-- ) {
+                if (A[i] < B[index]) {
+                    answer++;
+                    index--;
+                }
+            }
+
+            return answer;
+        }
+    }
 }
