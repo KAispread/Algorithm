eec1e40be85951d7e5971ecc536cf417fbae91ee
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-03-17 16:46:06 +0900

    📱DP: 점화식 세우기

---
+++ src/Baekjoon/Silver/Q1309DP.java
@@ -0,0 +1,34 @@
+package Baekjoon.Silver;
+
+import java.io.*;
+
+/*
+* 1309 - 동물원
+* */
+public class Q1309DP {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final int MOD = 9901;
+        int N = Integer.parseInt(br.readLine());
+
+        // dp[i][j] : i번째 줄의 j번째 칸에 동물을 놓을 수 있는 경우의 수
+        // j = 0 : 아무 동물도 놓지 않음
+        // j = 1 : 첫 번째 칸에 동물을 놓음
+        // j = 2 : 두 번째 칸에 동물을 놓음
+        long[][] dp = new long[N + 1][3];
+        dp[1][0] = dp[1][1] = dp[1][2] = 1; // 기저 사례
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+        }
+
+        long ans = (dp[N][0] + dp[N][1] + dp[N][2]) % MOD;
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
