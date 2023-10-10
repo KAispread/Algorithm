7ffd32e55ee9e7631800eec23f3643998e33c5d3
LeeKiWoo <pain0121@naver.com>
2023-10-10 15:07:36 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 260 ms, Memory: 19252 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/*
+* 1016번 - Gold I
+* 제곱이 아닌 수 찾기
+* */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long min = sc.nextLong();
+        long max = sc.nextLong();
+
+        boolean[] Check = new boolean[(int) (max - min + 1)];
+        for (long i = 2; i * i <= max; i++) {
+            long pow = i * i;
+            long start = min / pow;
+            if (min % pow != 0) {
+                start++;
+            }
+            for (long j = start; pow * j <= max; j++) {
+                Check[(int) ((j * pow) - min)] = true;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i <= max - min; i++) {
+            if (!Check[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
