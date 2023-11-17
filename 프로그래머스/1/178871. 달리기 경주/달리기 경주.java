3a99fb23bdbb7f82e9e0c652b297b3ce3518a6c8
LeeKiWoo <pain0121@naver.com>
2023-11-17 19:22:57 +0900

    [level 1] Title: 달리기 경주, Time: 0.08 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+
+   public String[] solution(String[] players, String[] callings) {
+
+
+        Map<String,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < players.length; i++){
+            String player = players[i];
+            map.put(player,i);
+        }
+
+        for(String calling : callings){
+            int index = map.get(calling);
+            String forwardCalling = players[index - 1];
+
+            players[index - 1] = calling;
+            map.put(calling,index-1);
+
+            players[index] = forwardCalling;
+            map.put(forwardCalling,index);
+        }
+        return players;
+    }
+
+    
+}
