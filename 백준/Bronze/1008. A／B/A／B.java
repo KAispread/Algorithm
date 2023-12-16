e60fe6069880f44dc38c3874372c6e02a17cb75a
LeeKiWoo <pain0121@naver.com>
2023-12-16 17:40:38 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    //
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        double v = (double) A / (double) B;
+
+        System.out.println(v);
+    }
+
+
+}
+
