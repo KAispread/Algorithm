25cbcbfee775a747bd4c04c5accdfc20ffd766af
LeeKiWoo <pain0121@naver.com>
2024-01-03 00:05:21 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -5,34 +5,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int answer = 0;
-
+    // 1 이 되는 경우 - (1)
+    // 2 가 되는 경우 - (1 + 1), (2)
+    // 3 이 되는 경우 - (1 + 1 + 1), (1 + 2), (2 + 1), (3)
+    // 4 가 되는 경우 - (3의 경우의 수 + 1) = 4 + (2의 경우의 수 + 2) = 2 + (1의 경우의 수 + 3) = 1 개 => 7개
+    // 결국 dp[N] = dp[N -1] + dp[N - 2] + dp[N - 3]
+    // dp[N - 3] 에는 + 3만 해주면 됨
+    // dp[N - 2] 에는 + 2만 해주면 됨
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         int T = Integer.parseInt(st.nextToken());
-        int[] dp = new int[11];
+        int[] dp = new int[12];
         dp[1] = 1;
         dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
 
         for (int t = 0; t < T; t++) {
-            answer = 0;
             int number = Integer.parseInt(bf.readLine());
-            DFS(0, number);
-            System.out.println(answer);
+            System.out.println(dp[number]);
         }
-    }
-
-    private static void DFS(int sum, int target) {
-        if (sum == target) {
-            answer++;
-            return;
-        }
-
-        if (sum + 1 <= target) DFS(sum + 1, target);
-        if (sum + 2 <= target) DFS(sum + 2, target);
-        if (sum + 3 <= target) DFS(sum + 3, target);
     }
 
 }
