ac53179fedece49fabd552d435e10e2b8d106e6f
LeeKiWoo <pain0121@naver.com>
2024-01-11 23:12:59 +0900

    [Gold IV] Title: 거짓말, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,75 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-/*
-* Gold IV - 1043번 - 거짓말쟁이가 되긴 싫어
-* */
 public class Main {
-    static int[] unionFind;
-    static List<Integer>[] patients;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int person = sc.nextInt();
-        int party = sc.nextInt();
-        int tNum = sc.nextInt();
-        if (tNum == 0) {
-            System.out.println(party);
+
+    static int[] union;
+    static List<Integer> knownPerson = new ArrayList<>();
+    static List<Integer>[] party;
+
+    // 이거 UNION FIND 집합 구하는 문제
+    // 모든 Party를 전부 순회하며 진실을 아는 사람과 함께하는 사람들은 모두 하나의 집합으로 묶음
+    // 다시 Party를 순회하여 각 파티에 참여한 모든 사람이 집합에 해당하지 않는다면 count + 1
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        union = new int[N + 1];
+        party = new List[M];
+
+        for (int i = 1; i < union.length; i++) {
+            union[i] = i;
+        }
+
+        for (int i = 0; i < party.length; i++) {
+            party[i] = new ArrayList<>();
+        }
+
+        st = new StringTokenizer(bf.readLine());
+        int knownCount = Integer.parseInt(st.nextToken());
+
+        if (knownCount == 0) {
+            System.out.println(M);
             return;
         }
 
-        unionFind = new int[person + 1];
-        for (int i = 1; i < unionFind.length; i++) {
-            unionFind[i] = i;
+        int knowner = Integer.parseInt(st.nextToken());
+        knownPerson.add(knowner);
+
+        while (st.hasMoreTokens()) {
+            int person = Integer.parseInt(st.nextToken());
+            union(knowner, person);
+            knownPerson.add(person);
         }
-        int[] truth = new int[tNum];
 
-        for (int i = 0; i < tNum; i++) {
-            truth[i] = sc.nextInt();
-        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int count = Integer.parseInt(st.nextToken());
 
-        int[] roots = new int[party];
-        int[] one = new int[party];
+            int person = Integer.parseInt(st.nextToken());
+            party[i].add(person);
 
-        for (int i = 0; i < party; i++) {
-            int number = sc.nextInt();
-
-            int first = sc.nextInt();
-            for (int j = 1; j < number; j++) {
-                int part = sc.nextInt();
-                union(first, part);
-                first = part;
+            for (int c = 1; c < count; c++) {
+                int anotherPerson = Integer.parseInt(st.nextToken());
+                party[i].add(anotherPerson);
+                union(person, anotherPerson);
             }
-
-            one[i] = first;
         }
 
+        int knownGroup = find(knownPerson.get(0));
         int answer = 0;
-        for (int i = 0; i < one.length; i++) {
+
+        for (int i = 0; i < party.length; i++) {
             boolean flag = true;
-            for (int j = 0; j < truth.length; j++) {
-                if (find(one[i]) == find(truth[j])) {
-                    flag = false;
-                }
+
+            for (int p : party[i]) {
+                if (find(p) == knownGroup) flag = false;
             }
-            if (flag) {
-                answer++;
-            }
+
+            if (flag) answer++;
         }
+
         System.out.println(answer);
     }
 
     private static void union(int a, int b) {
-        int A = find(a);
-        int B = find(b);
-        if (A != B) {
-            unionFind[B] = A;
+        int findA = find(a);
+        int findB = find(b);
+
+        if (findA != findB) {
+            union[findB] = findA;
         }
     }
 
     private static int find(int a) {
-        if (unionFind[a] == a) {
-            return a;
-        }
-        return unionFind[a] = find(unionFind[a]);
+        if (union[a] == a) return a;
+        return union[a] = find(union[a]);
     }
 }
+
