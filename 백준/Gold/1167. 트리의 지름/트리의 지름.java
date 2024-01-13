a6761405832e18ebc06383ed442f24203daa3677
LeeKiWoo <pain0121@naver.com>
2024-01-13 21:08:29 +0900

    [Gold II] Title: 트리의 지름, Time: 964 ms, Memory: 103524 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,76 +1,103 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
-    static int[] distance;
-    static ArrayList<Edge>[] A;
+
+    static class Node {
+        int number;
+        int length;
+
+        public Node(int number, int length) {
+            this.number = number;
+            this.length = length;
+        }
+    }
+
+    static List<Node>[] node;
+
+    // 1. 임의의 노드에서 가장 멀리 떨어진 노드 번호를 구함
+    // 2. 가장 멀리 떨어진 노드에서 다익스트라 알고리즘을 통해 가장 먼 거리를 구함
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int nodeNumber = Integer.parseInt(st.nextToken());
 
-        A = new ArrayList[nodeNumber + 1];
-        for (int i = 1; i < A.length; i++) {
-            A[i] = new ArrayList<>();
+        int count = Integer.parseInt(st.nextToken());
+        node = new List[count + 1];
+        for (int i = 1; i < node.length; i++) {
+            node[i] = new ArrayList<>();
         }
 
-        for (int count = 0; count < nodeNumber; count++) {
+        for (int i = 0; i < count; i++) {
             st = new StringTokenizer(bf.readLine());
-            int nodeNum = Integer.parseInt(st.nextToken());
+            int currentNode = Integer.parseInt(st.nextToken());
 
-            while (st.hasMoreTokens()) {
-                int forwardNode = Integer.parseInt(st.nextToken());
-                if (forwardNode == -1) {
-                    break;
-                }
-                int value = Integer.parseInt(st.nextToken());
-                A[nodeNum].add(new Edge(forwardNode, value));
+            while (true) {
+                int nextNode = Integer.parseInt(st.nextToken());
+                if (nextNode == -1) break;
+
+                int length = Integer.parseInt(st.nextToken());
+                node[currentNode].add(new Node(nextNode, length));
             }
         }
-        distance = new int[nodeNumber + 1];
-        visited = new boolean[nodeNumber + 1];
-        BFS(1);
+
+        int[] maxLength = dijicstra(1);
+        int nodeNumber = getMaxNode(maxLength);
+        int[] dijicstra = dijicstra(nodeNumber);
+
         int max = 0;
-        for (int i = 2; i <= nodeNumber; i++) {
-            if (distance[max] < distance[i]){
-                max = i;
+        for (int i = 1; i < dijicstra.length; i++) {
+            max = Math.max(dijicstra[i], max);
+        }
+
+        System.out.println(max);
+    }
+
+    private static int getMaxNode(int[] maxLength) {
+        int max = 0;
+        int maxNode = 0;
+        for (int i = 1; i < maxLength.length; i++) {
+            if (max < maxLength[i]) {
+                max = maxLength[i];
+                maxNode = i;
             }
         }
-        distance = new int[nodeNumber + 1];
-        visited = new boolean[nodeNumber + 1];
-        BFS(max);
-        Arrays.sort(distance);
-        System.out.println(distance[nodeNumber]);
+
+        return maxNode;
     }
-    
-    private static void BFS(int index) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(index);
-        visited[index] = true;
+
+    private static int[] dijicstra(int pivot) {
+        Queue<Node> queue = new PriorityQueue<>((o1, o2) -> {
+            return o2.length - o1.length;
+        });
+
+        boolean[] visited = new boolean[node.length];
+        int[] maximum = new int[node.length];
+
+        queue.offer(new Node(pivot, 0));
+
         while (!queue.isEmpty()) {
-            int nowNode = queue.poll();
-            for (Edge edge: A[nowNode]) {
-                int e = edge.e;
-                int v = edge.value;
-                if (!visited[e]) {
-                    visited[e] = true;
-                    queue.add(e);
-                    distance[e] = distance[nowNode] + v;
+            Node current = queue.poll();
+
+            if (visited[current.number]) continue;
+            visited[current.number] = true;
+
+            for (Node next : node[current.number]) {
+                if (!visited[next.number] && maximum[next.number] < current.length + next.length) {
+                    maximum[next.number] = Math.max(maximum[next.number], current.length + next.length);
+                    queue.offer(new Node(next.number, current.length + next.length));
                 }
             }
         }
-    }
-    
-    static class Edge {
-        int e;
-        int value;
 
-        public Edge(int e, int value) {
-            this.e = e;
-            this.value = value;
-        }
+        return maximum;
     }
 }
+
