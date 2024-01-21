545c76370da302d0418defd4f7e3cd592dbfe621
LeeKiWoo <pain0121@naver.com>
2024-01-21 16:18:20 +0900

    [Gold IV] Title: 플로이드, Time: 652 ms, Memory: 44964 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+* 11404번 - Gold IV
+* */
+public class Main {
+    static int[][] node;
+
+    static final int BIG_NUM = 10000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+
+        node = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                node[i][j] = BIG_NUM;
+            }
+        }
+        StringTokenizer st;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            node[start][end] = Math.min(cost, node[start][end]);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                for (int k = 1; k <=N; k++) {
+                    node[j][k] = Math.min(node[j][k], node[j][i] + node[i][k]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (node[i][j] >= BIG_NUM) {
+                    System.out.print("0 ");
+                    continue;
+                }
+                System.out.print(node[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
