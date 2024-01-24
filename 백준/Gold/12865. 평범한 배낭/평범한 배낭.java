8439f70cb831d32948965cac9cf5fe1f6bbfd7fa
LeeKiWoo <pain0121@naver.com>
2024-01-24 16:53:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 53596 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,44 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n,k;
-	static int dp[][],w[],v[]; // dp배열과 무게, 가치배열
-	
-	
-	public static void main(String[] args)   {
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		k = sc.nextInt();
-		
-		dp = new int[n+1][k+1];
-		
-		w =new int[n+1];
-		v = new int[n+1];
-		
-		for(int i=1;i<=n;i++) {			
-			w[i] = sc.nextInt();
-			v[i]= sc.nextInt();
-		
-		}
-		
-		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=k;j++) {
-				dp[i][j] = dp[i-1][j]; // 기본적으로 이전 아이템의 가치를 저장한다.
-				if(j - w[i]>=0) { // 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]); // 이전 아이템에서 구한 가치와 남은 무게의 가치 + 자신의 가치 중 큰 값을 취한다.
-				}
-			}
-		}
-		
-		System.out.println(dp[n][k]);
-		
-		
-		
-	}
-	
+
+    // knapsack Algorithm
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][K + 1];
+        int[] w = new int[N + 1];
+        int[] v = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            w[i] = weight;
+            v[i] = value;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j <= K; j++) {
+                dp[i][j] =  dp[i-1][j];
+                int remain = j - w[i];
+
+                if (remain >= 0) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][remain] + v[i]);
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+
 }
+
