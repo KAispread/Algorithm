15ddd94202ea6396c49ccd40eeb2d5b3d87ddc19
LeeKiWoo <pain0121@naver.com>
2024-03-21 23:24:56 +0900

    [Gold III] Title: 웜홀, Time: 396 ms, Memory: 47636 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -8,51 +8,61 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Node {
+    static class Edge {
         int number;
-        int length;
+        int dist;
 
-        public Node(int number, int length) {
+        public Edge(int number, int dist) {
             this.number = number;
-            this.length = length;
+            this.dist = dist;
         }
     }
 
-    static List<Node>[] node;
-    static final int INF = 987654321;
+    static List<Edge>[] nodes;
+    static final int MAX = 987654321;
 
-    // bellmanFord
+    // Bellman ford
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        int T = Integer.parseInt(st.nextToken());
-
-        for (int t = 0; t < T; t++) {
+        int testCase = Integer.parseInt(st.nextToken());
+        for (int t = 0; t < testCase; t++) {
             st = new StringTokenizer(bf.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
 
-            node = new List[N + 1];
-            for (int i = 1; i < node.length; i++) {
-                node[i] = new ArrayList<>();
+            int nodesNumber = Integer.parseInt(st.nextToken());
+            int roadsNumber = Integer.parseInt(st.nextToken());
+            int holeNumber = Integer.parseInt(st.nextToken());
+
+            nodes = new List[nodesNumber + 1];
+
+            for (int i = 1; i < nodes.length; i++) {
+                nodes[i] = new ArrayList<>();
             }
 
-            for (int i = 0; i < M + W; i++) {
+            for (int i = 0; i < roadsNumber + holeNumber; i++) {
+                // 도로일경우
+                if (i < roadsNumber) {
+                    st = new StringTokenizer(bf.readLine());
+
+                    int nodeA = Integer.parseInt(st.nextToken());
+                    int nodeB = Integer.parseInt(st.nextToken());
+                    int dist = Integer.parseInt(st.nextToken());
+
+                    nodes[nodeA].add(new Edge(nodeB, dist));
+                    nodes[nodeB].add(new Edge(nodeA, dist));
+
+                    continue;
+                }
+
+                // 웜홀일경우
                 st = new StringTokenizer(bf.readLine());
 
                 int nodeA = Integer.parseInt(st.nextToken());
                 int nodeB = Integer.parseInt(st.nextToken());
-                int length = Integer.parseInt(st.nextToken());
+                int back = Integer.parseInt(st.nextToken());
 
-                if (i >= M) {
-                    node[nodeA].add(new Node(nodeB, -length));
-                    continue;
-                }
-
-                node[nodeA].add(new Node(nodeB, length));
-                node[nodeB].add(new Node(nodeA, length));
+                nodes[nodeA].add(new Edge(nodeB, -back));
             }
 
             if (bellmanFord(1)) {
@@ -64,36 +74,30 @@ public class Main {
     }
 
     private static boolean bellmanFord(int start) {
-        int[] minimum = new int[node.length];
-        Arrays.fill(minimum, INF);
+        int[] minimum = new int[nodes.length];
+        Arrays.fill(minimum, MAX);
         minimum[start] = 0;
-        boolean update = false;
 
-        // 전체 노드의 수보다 1 적게 반복
-        for (int i = 1; i < minimum.length - 1; i++) {
-            update = false;
+        for (int i = 1; i < nodes.length - 1; i++) {
 
-            // 모든 노드를 순회하며 노드 값 update
-            for (int j = 1; j < node.length; j++) {
-                for (Node next : node[j]) {
-                    if (minimum[next.number] > minimum[j] + next.length) {
-                        minimum[next.number] = minimum[j] + next.length;
+            boolean update = false;
+            for (int j = 1; j < nodes.length; j++) {
+                for (Edge next : nodes[j]) {
+                    if (minimum[next.number] > next.dist + minimum[j]) {
+                        minimum[next.number] = next.dist + minimum[j];
                         update = true;
                     }
                 }
             }
 
-            if (!update) {
-                break;
-            }
-        }
+            if (!update) break;
 
-        if (update) {
-            // 업데이트가 발생한다면 음수 사이클이 있는 것
-            for (int j = 1; j < node.length; j++) {
-                for (Node next : node[j]) {
-                    if (minimum[next.number] > minimum[j] + next.length) {
-                        return true;
+            if (i == nodes.length - 2) {
+                for (int j = 1; j < nodes.length; j++) {
+                    for (Edge next : nodes[j]) {
+                        if (minimum[next.number] > next.dist + minimum[j]) {
+                            return true;
+                        }
                     }
                 }
             }
@@ -102,4 +106,3 @@ public class Main {
         return false;
     }
 }
-
